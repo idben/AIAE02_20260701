38c4e403a4f5ce2d4623fdc4dbdbc803b9fa3b23
Ben Chen <idben.chen@gmail.com>
2026-07-01 15:12:41 +0800

    數字型別及其方法

---
+++ num.py
@@ -0,0 +1,8 @@
+print(round(1.5)) # (銀行家)四捨五入
+print(round(2.5))
+print(round(3.5))
+print(round(4.5))
+
+print(f"\n1, 5, 3 取最大值 {max(1, 5, 3)}")
+print(f"\n1, 5, 3 取最小值 {min(1, 5, 3)}")
+print(f"\n1, 5, 3 加總 {sum([1, 5, 3])}")

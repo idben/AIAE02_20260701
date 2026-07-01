a8c88397a012ebdcaed8636c9cc7b1fb50a7a6de
Ben Chen <idben.chen@gmail.com>
2026-07-01 14:06:12 +0800

    列表

---
+++ list.py
@@ -0,0 +1,14 @@
+list1 = [1, 2, 3]
+
+
+
+list1[1] = "appple"
+list1.append(5566998)
+
+print(list1)
+print(list1[0]) # 取索引, 索引值
+print(list1[1]) # 索引值從 0 開始計算
+print(list1[2])
+
+print(len(list1)) # 計算列表長度
+print(len(list1[1]))

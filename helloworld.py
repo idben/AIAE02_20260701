a2ccc4330de7bd89f52330c444c4b4c6692e5ddb
Ben Chen <idben.chen@gmail.com>
2026-07-01 10:50:21 +0800

    變數使用

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,5 @@
 print("你好")
 
+a = 10
+b = 20
+print(a + b)
---
+++ vars.py
@@ -0,0 +1,13 @@
+age = 18
+name = 'Ben Chen'
+height = 170.5
+is_login = True # 下底線命名方式 蛇形命名方式
+isAdmin = False
+# 小駝峰命名方式
+
+
+
+print(age)
+print(name)
+print(height)
+print(is_login)

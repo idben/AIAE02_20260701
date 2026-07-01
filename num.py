6a2519a53165114fee3054048318a4feeb7f15cb
Ben Chen <idben.chen@gmail.com>
2026-07-01 15:28:36 +0800

    無條件捨棄, 無條件進位

--- num.py
+++ num.py
@@ -1,3 +1,5 @@
+import math
+
 print(round(1.5)) # (銀行家)四捨五入
 print(round(2.5))
 print(round(3.5))
@@ -6,3 +8,9 @@ print(round(4.5))
 print(f"\n1, 5, 3 取最大值 {max(1, 5, 3)}")
 print(f"\n1, 5, 3 取最小值 {min(1, 5, 3)}")
 print(f"\n1, 5, 3 加總 {sum([1, 5, 3])}")
+
+# 無條件捨棄
+print(int(4.3))
+
+print(math.floor(4.3))
+print(math.ceil(4.3))
